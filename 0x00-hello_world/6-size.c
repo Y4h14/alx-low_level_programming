f671b1bf5ec86b7c1795bd1edf027a2424643efa
yahia <yahyasadig2013@gmail.com>
2023-08-11 11:13:34 +0300

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,11 +13,11 @@ long long int doubleType;
 char charType;
 float floatType;
 
-printf("Size of char: %lu byte\n",(unsigned long)sizeof(charType));
-printf("Size of an int: %lu bytes\n", (unsigned long)sizeof(intType));
-printf("Size of a long int: %lu bytes\n", (unsigned long)sizeof(longIntType));
-printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(doubleType));
-printf("Size of an float: %lu bytes\n", (unsigned long)sizeof(floatType));
+printf("Size of char: %lu byte(s)\n",(unsigned long)sizeof(charType));
+printf("Size of an int: %lu  byte(s)\n", (unsigned long)sizeof(intType));
+printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(longIntType));
+printf("Size of a long long int: %lu  byte(s)\n", (unsigned long)sizeof(doubleType));
+printf("Size of an float: %lu  byte(s)\n", (unsigned long)sizeof(floatType));
 
 return (0);
 }
