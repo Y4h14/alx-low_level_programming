a3d0b480dc11699193dc71162e3ece17a23fafdd
yahia <yahyasadig2013@gmail.com>
2023-08-13 10:03:59 +0300

    u

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,34 +9,23 @@
 */
 int main(void)
 {
-int i , j , k , l ; 
-for (i = 0;i <= 9; i++)
+int i, j;
+for (i = 0; i <= 99; i++)
 {
-    for (j = 0;j <= 9; j++)
-    {
-        for (k = 0; k <= 9; k++)
-        {
-            for (l = 1; l <= 9; l++)
-            {
-                if (k+l == i+j)
-                {
-                    continue;
-                }
-                
-                putchar('0'+ i);
-                putchar('0'+j);
-                putchar(' ');
-
-                putchar('0'+ k);
-                putchar('0'+ l);
-                putchar(',');
-
-                
-                
-            }   
-        }
-    }
+	for (j = i + 1; j <= 99; j++)
+	{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+	if (i < 98 || j < 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
 }
 putchar('\n');
-return (0); 
+return (0);
 }
