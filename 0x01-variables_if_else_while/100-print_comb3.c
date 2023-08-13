5898214a1d5a5c47dccab22cfa32220722ba6022
yahia <yahyasadig2013@gmail.com>
2023-08-13 08:41:42 +0300

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Description: print the value of n's status(+,-,0)
+ * Description: print number combinarions
  *
  * Return: Always 0 (success)
  *
@@ -11,26 +11,23 @@ int main(void)
 {
 int j;
 int i;
-for (i = 0;i <= 8; i++)
+for (i = 0; i <= 8; i++)
 {
-    for (j = 0;j <= 9; j++)
-    {
-        if (i == j || (i*10 + j)%10 == 0)
-        {
-            continue;
-        }
-       
-        
-        putchar('0'+ i);
-        putchar('0'+j);
-        if(i + j != 17)
-        {
-            putchar(',');
-            putchar(' ');
-        }   
-    }
+	for (j = 0; j <= 9; j++)
+	{
+		if (i == j || i > j)
+		{
+			continue;
+		}
+		putchar('0' + i);
+		putchar('0' + j);
+		if (i + j != 17)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 }
-
 putchar('\n');
-return (0); 
+return (0);
 }
