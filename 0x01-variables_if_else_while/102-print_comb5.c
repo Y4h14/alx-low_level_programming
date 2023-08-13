69cac6177f0211d8d4a6a5e238b383df931f6b3e
yahia <yahyasadig2013@gmail.com>
2023-08-13 09:21:56 +0300

    u

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,42 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Description: print the value of n's status(+,-,0)
+ * Description: print two two digit combinations
  *
  * Return: Always 0 (success)
  *
 */
 int main(void)
 {
-int j;
-int i;
+int i , j , k , l ; 
 for (i = 0;i <= 9; i++)
 {
     for (j = 0;j <= 9; j++)
     {
-        
-        
-        putchar('0'+ i);
-        putchar('0'+j);
-        
-        putchar(',');
-        putchar(' ');
+        for (k = 0; k <= 9; k++)
+        {
+            for (l = 1; l <= 9; l++)
+            {
+                if (k+l == i+j)
+                {
+                    continue;
+                }
+                
+                putchar('0'+ i);
+                putchar('0'+j);
+                putchar(' ');
+
+                putchar('0'+ k);
+                putchar('0'+ l);
+                putchar(',');
+
+                
+                
+            }   
+        }
     }
-    
-    
 }
-
 putchar('\n');
-
-
 return (0); 
 }
