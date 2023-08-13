b1c97a4b7b58e483e07196028487348a545f3468
yahia <yahyasadig2013@gmail.com>
2023-08-13 08:58:48 +0300

    updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Description: print the value of n's status(+,-,0)
+ * Description: print the combinations of 3 numbers.
  *
  * Return: Always 0 (success)
  *
@@ -12,33 +12,34 @@ int main(void)
 int j;
 int i;
 int k;
-for (i = 0;i <= 7; i++)
+for (i = 0; i <= 7; i++)
 {
-    for (j = 1;j <= 8; j++)
-    {
-        for (k = 2; k <= 9; k++)
-        {
-           
-        
-        putchar('0'+ i);
-        putchar('0'+j);
-        putchar('0'+k);
-        if(k == 9)
-        {
-            continue;
-        }
-        putchar(',');
-        putchar(' '); 
-        }
-        
-        
-    }
-    
-    
+	for (j = 1; j <= 8; j++)
+	{
+	if (i == j || i > j)
+	{
+		continue;
+	}
+		for (k = 2; k <= 9; k++)
+		{
+			if ((j == k) || (j > k))
+			{
+				continue;
+			}
+
+			putchar('0' + j);
+			putchar('0' + i);
+			putchar('0' + k);
+			if (i + j + k == 7 + 8 + 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
 }
-
 putchar('\n');
-
-
-return (0); 
+return (0);
 }
