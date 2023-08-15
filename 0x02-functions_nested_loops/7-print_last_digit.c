ce56a5893f9bb8d3f885f3a7302b47a4d1c2e9b6
yahia <yahyasadig2013@gmail.com>
2023-08-15 16:51:44 +0300

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,9 @@
 int print_last_digit(int x)
 {
 int y ;
-if (x > 10)
-{
-     y = (x % 10) + '0'; 
-}
-else
-{
-    y = x + '0'; 
-}
 
+     y = (x % 10) ;
+     
+_putchar(y + '0'); 
 return (y);
 }
