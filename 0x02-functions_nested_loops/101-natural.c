58cd49d9489136d4e3cc5161176493b8c0321499
yahia <yahyasadig2013@gmail.com>
2023-08-15 20:11:41 +0300

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - sums number divisibal by 5,3
 *
-* Rerturn: always 0 
+* Return: always 0
 */
-
 int main(void)
 {
 int sum = 0;
-int i = 0; 
-
+int i = 0;
 while (i < 1024)
 {
-    if ((i % 5) == 0|| (i % 3) == 0)
-    {
-        sum += i; 
-    }
-    
-    i++;
+	if ((i % 5) == 0 || (i % 3) == 0)
+	{
+		sum += i;
+	}
+	i++;
 }
-
 printf("%d\n", sum);
-
-
-return (0); 
+return (0);
 }
