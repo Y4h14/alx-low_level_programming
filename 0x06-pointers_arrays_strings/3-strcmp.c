9d33c552a08ddd2e0264bb1ccea331494ae46c6b
yahia <yahyasadig2013@gmail.com>
2023-08-25 20:02:42 +0300

    updated task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,18 @@
 int _strcmp(char *s1, char *s2)
 {
 while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-{
+	{
+		s1++;
+		s2++;
+	}
+
 	if (*s1 == *s2)
 	{
-	return (0);
+		return (0);
 	}
-	s1++;
-	s2++;
-}
-return (*s1 - *s2);
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
