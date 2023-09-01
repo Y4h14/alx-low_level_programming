21acc21bc4ccdb427ac7919f562dbaef61c35758
yahia <yahyasadig2013@gmail.com>
2023-09-01 15:40:36 +0300

    updated tasks 1 and 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,23 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-int sum = 0;
-int x;
+int i, n, sum = 0;
+char *flag;
+
 if (argc < 2)
 {
 	printf("0\n");
 	return (0);
 }
-for (i = 1; i <= argc; i++)
+
+for (i = 1; argv[i]; i++)
 {
-	x = atoi(argv[i]);
-	if (!x)
-	{
+	n = strtol(argv[i], &flag, 10);
+  	if (*flag)
+  	{
 		printf("Error\n");
 		return (1);
 	}
-	sum += atoi(argv[i]);
+	else
+	{
+		sum += n;
+	}
 }
 printf("%d\n", sum);
 return (0);
