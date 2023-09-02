d6fd9fd6758bf669168d1886bd4f096eae71d7cf
yahia <yahyasadig2013@gmail.com>
2023-09-02 04:08:50 +0300

    updated betty styling

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * _putchar - print a charecter
+ * @c: a char
+ * Return: always 0;
+ */
 int _putchar(char c)
 {
 extern long write(int, const char *, unsigned long);
-(void )write(1,&c,1);
-return (0); 
+(void)write(1, &c, 1);
+return (0);
 }
