705b7bf1a7ee749536c991505c6fba14bf658a96
yahia <yahyasadig2013@gmail.com>
2023-09-11 17:22:10 +0300

    rewrote code for new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+int len(char *s);
 /**
  * new_dog - creates a new dog
  * @name: dog's name
@@ -10,18 +11,47 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *newdog;
 char *n, *o;
+int lend, leno, i, j;
+lend = len(name);
+leno = len(owner);
 
 newdog = malloc(sizeof(struct dog));
 if (newdog == NULL)
 	return (NULL);
-if (name == NULL)
+
+if (name == NULL || owner == NULL)
 	return (NULL);
-if (owner == NULL)
+
+n = malloc(lend *sizeof(char));
+if (n == NULL)
 	return (NULL);
-n = name;
-o = owner;
+o = malloc(leno *sizeof(char));
+if (o == NULL)
+	return (NULL);
+
+for (i = 0; name[i] != '\0'; i++)
+	n[i] = name[i];
+n[i] = '\0';
+for (j = 0; owner[j] != '\0'; j++) 
+	o[i] = owner[i];
+o[i] = '\0';
+
 newdog->name = n;
 newdog->age = age;
 newdog->owner = o;
 return (newdog);
+}
+/**
+ * len - calculates the lenth of a string
+ * @s: a string of char
+ * Returns: lenth of the string
+ */
+int len(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	i++;
+}
+return (i);
 }
