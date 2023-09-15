ee5952475e97003627c2f80eb6488942f395ab23
yahia <yahyasadig2013@gmail.com>
2023-09-15 13:37:06 +0300

    updated betty styling

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,18 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i;
 
-if (n == 0)
+if (n != 0)
 {
-	printf("\n");
-	return;
-}
-va_start(args, n);
+	va_start(args, n);
 
-for (i = 0; i < n; i++)
-{
-	printf("%d", va_arg(args, int));
-	if (i < n - 1 && separator != NULL)
-		printf("%s", separator);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 }
 va_end(args);
 printf("\n");
