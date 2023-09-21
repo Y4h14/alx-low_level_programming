e251bd803451c00b54fc6d9f05d00ee2a79f530c
yahia <yahyasadig2013@gmail.com>
2023-09-21 21:30:52 +0300

    debuged advanced tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
-section .dss
+extern printf ;importing printf
 
 section .data
-	hello: db "Hello, Holberton",20
-	hollolen: equ $-hello
+	hello: db "Hello, Holberton",0
+	fmt: db "%s", 10, 0
 
 section .text
-	global_start
+	global main
 
-_start:
-mov rax,1
-mov rdi,1
+main:
+push rbp
+mov rdi,fmt
 mov rsi,hello
-mov rdx,hollolen
-syscall
+mov rax,0
+call printf
+pop rbp
 
-mov rax,60
-mov rdi,0
-syscall
+mov rax,0
+ret
