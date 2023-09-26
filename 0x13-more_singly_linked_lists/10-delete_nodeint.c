0bd7f2914ed7ee5092835d3a71ae0184a3cd3752
Y4h14 <yahyasadig2013@gmail.com>
2023-09-26 19:23:21 +0300

    !

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *curr, *temp, *target;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL || index < 0)
 		return (-1);
 
 	curr = *head;
