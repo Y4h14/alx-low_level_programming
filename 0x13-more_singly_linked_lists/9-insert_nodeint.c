02bcec123743714cc738397bf5513890c304ea3a
Y4h14 <yahyasadig2013@gmail.com>
2023-09-26 20:14:06 +0300

    !

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (curr)
 	{
-		if (i == idx)
+		if (i == idx - 1)
 		{
 			new->next = curr->next;
 			curr->next = new;
