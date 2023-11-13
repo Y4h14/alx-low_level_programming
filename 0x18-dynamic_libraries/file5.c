e7a5d23e15f9dd99ec8d06dea9339f796d8cb2ec
Y4h14 <yahyasadig2013@gmail.com>
2023-11-13 18:35:02 +0300

    task 2

---
+++ 0x18-dynamic_libraries/file5.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b:second number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * add - subtract two numbers
+ * @a: first number
+ * @b:second number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mult - multiply two numbers
+ * @a: first number
+ * @b:second number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b:second number
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - calculate the mod of two numbers
+ * @a: first number
+ * @b:second number
+ * Return: a / b
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
