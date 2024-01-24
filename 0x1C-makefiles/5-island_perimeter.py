7d2f33d80e900c74c34db0265837e34b5df5af30
Y4h14 <yahyasadig2013@gmail.com>
2024-01-25 00:49:18 +0300

    Added tasks 0 to 5 and 100

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """"returs the land perimeter"""
+    total = 0
+    for i in range(len(grid)):
+        for j in grid[i]:
+            if grid[i][j] == 1:
+                total += 4
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+                if grid[i-1][j] == 1:
+                    total -= 2
+                if grid[i][j-1] == 1:
+                    total -= 2
+    return total
