ccb3ff8b1a0e3a2df212eda1a88ec809a236fc2f
Y4h14 <yahyasadig2013@gmail.com>
2024-01-25 01:15:23 +0300

    !

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,12 +6,12 @@ def island_perimeter(grid):
     """"returs the land perimeter"""
     total = 0
     for i in range(len(grid)):
-        for j in grid[i]:
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 total += 4
-            if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
-                if grid[i-1][j] == 1:
-                    total -= 2
-                if grid[i][j-1] == 1:
-                    total -= 2
+                if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+                    if grid[i-1][j] == 1:
+                        total -= 2
+                    if grid[i][j-1] == 1:
+                        total -= 2
     return total
